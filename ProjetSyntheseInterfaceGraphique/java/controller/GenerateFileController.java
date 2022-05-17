272755757af5e0a633689bc30f07f1d859075e30
Valaaz <val.zancoth@gmail.com>
2022-05-17 19:52:22 +0200

    separate fxml

---
+++ ProjetSyntheseInterfaceGraphique/java/controller/GenerateFileController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+public class GenerateFileController {
+    @FXML
+    private Label lblFileChoosen;
+
+    @FXML
+    private Pane panePoints;
+
+    @FXML
+    void reset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void validate(ActionEvent event) {
+
+    }
+
+}
